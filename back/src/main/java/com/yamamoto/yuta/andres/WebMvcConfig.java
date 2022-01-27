63250cea9bd373502574509e33ac996e0bf6dc00
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-27 22:12:26 +0900

    add CORS

---
+++ back/src/main/java/com/yamamoto/yuta/andres/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.yamamoto.yuta.andres;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:4200", "https://y-chip.github.io")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}
