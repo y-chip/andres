78dbb1d4b005ad6249c94db416cba81a401baed9
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-28 01:26:59 +0900

    remove unnecessary component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -98,35 +98,6 @@ import { AppConfigComponent } from './app.config.component';
 import { AppMenuComponent } from './app.menu.component';
 import { AppMenuitemComponent } from './app.menuitem.component';
 
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FormLayoutComponent } from './components/formlayout/formlayout.component';
-import { FloatLabelComponent } from './components/floatlabel/floatlabel.component';
-import { InvalidStateComponent } from './components/invalidstate/invalidstate.component';
-import { InputComponent } from './components/input/input.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TableComponent } from './components/table/table.component';
-import { ListComponent } from './components/list/list.component';
-import { TreeComponent } from './components/tree/tree.component';
-import { PanelsComponent } from './components/panels/panels.component';
-import { OverlaysComponent } from './components/overlays/overlays.component';
-import { MediaComponent } from './components/media/media.component';
-import { MenusComponent } from './components/menus/menus.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { MiscComponent } from './components/misc/misc.component';
-import { EmptyComponent } from './components/empty/empty.component';
-import { ChartsComponent } from './components/charts/charts.component';
-import { FileComponent } from './components/file/file.component';
-import { DocumentationComponent } from './components/documentation/documentation.component';
-import { CrudComponent } from './components/crud/crud.component';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { IconsComponent } from './components/icons/icons.component';
-import { BlocksComponent } from './components/blocks/blocks.component';
-import { PaymentComponent } from './components/menus/payment.component';
-import { ConfirmationComponent } from './components/menus/confirmation.component';
-import { PersonalComponent } from './components/menus/personal.component';
-import { SeatComponent } from './components/menus/seat.component';
-import { LandingComponent } from './components/landing/landing.component';
-
 import { CountryService } from './service/countryservice';
 import { CustomerService } from './service/customerservice';
 import { EventService } from './service/eventservice';
@@ -136,10 +107,6 @@ import { PhotoService } from './service/photoservice';
 import { ProductService } from './service/productservice';
 import { MenuService } from './service/app.menu.service';
 import { ConfigService } from './service/app.config.service';
-import { LoginComponent } from './components/login/login.component';
-import { ErrorComponent } from './components/error/error.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
-import { AccessComponent } from './components/access/access.component';
 import { SampleFormComponent } from './pages/sample-form/sample-form.component';
 import { ComponentModule } from './components/component.module';
 
@@ -242,40 +209,7 @@ import { ComponentModule } from './components/component.module';
         AppConfigComponent,
         AppMenuComponent,
         AppMenuitemComponent,
-        DashboardComponent,
-        FormLayoutComponent,
-        FloatLabelComponent,
-        InvalidStateComponent,
-        InputComponent,
-        ButtonComponent,
-        TableComponent,
-        ListComponent,
-        TreeComponent,
-        PanelsComponent,
-        OverlaysComponent,
-        MenusComponent,
-        MessagesComponent,
-        MessagesComponent,
-        MiscComponent,
-        ChartsComponent,
-        EmptyComponent,
-        FileComponent,
-        IconsComponent,
-        DocumentationComponent,
-        CrudComponent,
-        TimelineComponent,
-        BlocksComponent,
         BlockViewer,
-        MediaComponent,
-        PaymentComponent,
-        ConfirmationComponent,
-        PersonalComponent,
-        SeatComponent,
-        LandingComponent,
-        LoginComponent,
-        ErrorComponent,
-        NotfoundComponent,
-        AccessComponent,
         SampleFormComponent,
     ],
     providers: [
