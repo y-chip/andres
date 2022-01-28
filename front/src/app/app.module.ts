3c6d653d32fea9a0177b417551febda233196198
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-28 15:54:37 +0900

    refactor package

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ComponentModule } from './components/component.module';
+import { ComponentModule } from './component/component.module';
 
 import { AppComponent } from './app.component';
 import { AppMainComponent } from './app.main.component';
