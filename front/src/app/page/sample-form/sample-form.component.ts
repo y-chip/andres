950b347ab1efc68d629a962e56d96663d301bd54
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-28 23:17:59 +0900

    remove unnecessary file

--- front/src/app/page/sample-form/sample-form.component.ts
+++ front/src/app/page/sample-form/sample-form.component.ts
@@ -7,7 +7,6 @@ import { SampleForm } from '../../../gen/model/sampleForm';
 @Component({
     selector: 'app-sample-form',
     templateUrl: './sample-form.component.html',
-    styleUrls: ['./sample-form.component.scss'],
 })
 export class SampleFormComponent extends PageComponent implements OnInit {
     sampleForm: SampleForm | undefined;
