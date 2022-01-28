b5e026cd4a14bc63a07e9bb3f2cc9384fe6fcdab
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-28 20:35:58 +0900

    add MyBatis

--- back/src/main/java/com/yamamoto/yuta/andres/AndresApplication.java
+++ back/src/main/java/com/yamamoto/yuta/andres/AndresApplication.java
@@ -1,8 +1,10 @@
 package com.yamamoto.yuta.andres;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.yamamoto.yuta.andres.infrastructure.repository.mapper")
 @SpringBootApplication
 public class AndresApplication {
 
