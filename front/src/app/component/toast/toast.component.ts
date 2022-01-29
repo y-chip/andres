494c89c7f5391821ed288e3baf72525ed3d5b612
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-29 22:41:58 +0900

    fix ToastComponent z-index

--- front/src/app/component/toast/toast.component.ts
+++ front/src/app/component/toast/toast.component.ts
@@ -3,6 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'and-toast',
     templateUrl: './toast.component.html',
+    styles: [
+        `
+            :host ::ng-deep .p-toast {
+                z-index: 998;
+            }
+        `,
+    ],
 })
 export class ToastComponent implements OnInit {
     constructor() {}
