7e376e97e2c8d4aa7ac19ee11601e8a7252910b5
Yuta Yamamoto <wookiee33@gmail.com>
2022-01-31 18:07:54 +0900

    remove log

--- front/src/app/http-interceptors/convert-date-interceptor.ts
+++ front/src/app/http-interceptors/convert-date-interceptor.ts
@@ -17,7 +17,6 @@ export class ConvertDateInterceptor implements HttpInterceptor {
         req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        console.log('here');
         return next.handle(req).pipe(
             map((val: HttpEvent<any>) => {
                 if (val instanceof HttpResponse) {
